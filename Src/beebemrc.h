05a5758c8ab49cd29bce30376bfcd41bc0fda8d8
Steve Insley <steve@insley.org>
2019-01-30 00:22:31 +0000

    Updated _APS_NEXT_COMMAND_VALUE to allow merge of pause-emulation branch.

--- Src/beebemrc.h
+++ Src/beebemrc.h
@@ -435,7 +435,7 @@ Boston, MA  02110-1301, USA.
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        117
-#define _APS_NEXT_COMMAND_VALUE         40293
+#define _APS_NEXT_COMMAND_VALUE         40294
 #define _APS_NEXT_CONTROL_VALUE         1087
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
